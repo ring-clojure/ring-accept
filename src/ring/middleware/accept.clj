e1a4d2596d8a32269ee75f0819af19bfb62c827c
James Reeves <jreeves@weavejester.com>
2014-12-03 17:52:30 +0000

    Parse the qvalue separately to the range params

--- src/ring/middleware/accept.clj
+++ src/ring/middleware/accept.clj
@@ -2,25 +2,27 @@
   (:require [clojure.string :as str]
             [ring.util.parsing :as p]))
 
-(def ^:private re-parameter
-  (re-pattern (str "\\s*;\\s*(" p/re-token ")=(" p/re-value ")")))
-
 (def ^:private re-wildcard #"\*")
+
+(def ^:private re-qvalue #"0(?:\.\d{0,3})?|1(?:\.0{0,3})?")
+
+(def ^:private re-parameter
+  (re-pattern (str "\\s*;\\s*" p/re-token "=" p/re-value)))
+
+(def ^:private re-accept-params
+  (re-pattern (str "\\s*;\\s*" "q=(" re-qvalue ")")))
 
 (def ^:private re-media-range
   (re-pattern
    (str "(" re-wildcard "/" re-wildcard
         "|" p/re-token  "/" re-wildcard
-        "|" p/re-token  "/" p/re-token ")"
-        "((?:" re-parameter ")*)")))
-
-(defn- parse-parameters [params]
-  (into {} (for [[_ name value] (re-seq re-parameter params)]
-             [name value])))
+        "|" p/re-token  "/" p/re-token
+        "(?:" re-parameter ")*?" ")"
+        "(?:" re-accept-params ")?")))
 
 (defn- parse-media-range [media-range]
-  (let [[_ range params] (re-matches re-media-range media-range)]
-    [range (parse-parameters params)]))
+  (let [[_ range _ qvalue] (re-matches re-media-range media-range)]
+    {range (Double/parseDouble qvalue)}))
 
 (defn wrap-accept [handler]
   (fn [request]
