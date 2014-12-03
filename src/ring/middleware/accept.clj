02a939eb1931fa5bbb237d37d9c68ff061a10be7
James Reeves <jreeves@weavejester.com>
2014-12-03 21:05:46 +0000

    Add parsing of multiple accept values

--- src/ring/middleware/accept.clj
+++ src/ring/middleware/accept.clj
@@ -13,16 +13,25 @@
   (re-pattern (str "\\s*;\\s*" "q=(" re-qvalue ")")))
 
 (def ^:private re-media-range
-  (re-pattern
-   (str "(" re-wildcard "/" re-wildcard
-        "|" p/re-token  "/" re-wildcard
-        "|" p/re-token  "/" p/re-token
-        "(?:" re-parameter ")*?" ")"
-        "(?:" re-accept-params ")?")))
+  (re-pattern (str "(" re-wildcard "/" re-wildcard
+                   "|" p/re-token  "/" re-wildcard
+                   "|" p/re-token  "/" p/re-token ")"
+                   "((?:" re-parameter ")*?)")))
 
-(defn- parse-media-range [media-range]
-  (let [[_ range _ qvalue] (re-matches re-media-range media-range)]
-    {range (Double/parseDouble qvalue)}))
+(def ^:private re-accept-value
+  (re-pattern (str "(" re-media-range ")"
+                   "(?:(" re-accept-params ")"
+                   "(" re-parameter ")*)?")))
+
+(def ^:private re-accept-header
+  (re-pattern (str "(?<=^|,)\\s*(" re-media-range ")\\s*(?:,|$)")))
+
+(defn- parse-accept [header-value]
+  (let [[_ type _ _ _ _ q] (re-matches re-accept-value header-value)]
+    {type (Double/parseDouble (or q "1"))}))
+
+(defn- parse-header [header]
+  (map second (re-seq re-accept-header header)))
 
 (defn wrap-accept [handler]
   (fn [request]
