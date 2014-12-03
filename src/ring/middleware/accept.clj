9677c884108c0698760d3cd6e57cf54e7ec13df0
James Reeves <jreeves@weavejester.com>
2014-12-03 16:50:31 +0000

    Add basic parsing of media-ranges

--- src/ring/middleware/accept.clj
+++ src/ring/middleware/accept.clj
@@ -1,4 +1,26 @@
-(ns ring.middleware.accept)
+(ns ring.middleware.accept
+  (:require [clojure.string :as str]
+            [ring.util.parsing :as p]))
+
+(def ^:private re-parameter
+  (re-pattern (str "\\s*;\\s*(" p/re-token ")=(" p/re-value ")")))
+
+(def ^:private re-wildcard #"\*")
+
+(def ^:private re-media-range
+  (re-pattern
+   (str "(" re-wildcard "/" re-wildcard
+        "|" p/re-token  "/" re-wildcard
+        "|" p/re-token  "/" p/re-token ")"
+        "((?:" re-parameter ")*)")))
+
+(defn- parse-parameters [params]
+  (into {} (for [[_ name value] (re-seq re-parameter params)]
+             [name value])))
+
+(defn- parse-media-range [media-range]
+  (let [[_ range params] (re-matches re-media-range media-range)]
+    [range (parse-parameters params)]))
 
 (defn wrap-accept [handler]
   (fn [request]
